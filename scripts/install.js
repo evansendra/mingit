674936e2381701570e1b8c63532985e70647867f
Evan <code@evansendra.com>
2016-07-22 00:50:06 +0200

    simplify install for test publish

--- scripts/install.js
+++ scripts/install.js
@@ -1,15 +1,1 @@
-function isWindows() {
-    return os.platform().toLowerCase().indexOf('win32') > -1;
-}
-
-function isApple() {
-    return os.platform().toLowerCase().indexOf('darwin') > -1;
-}
-
-if (isWindows()) {
-    exec('./install-win-bashrc.sh');
-} else if (isApple()) {
-    exec('./install-apple-bash-profile.sh');
-} else {
-    exec('./install-linux-bashrc.sh');
-}
+console.log('installed!');
