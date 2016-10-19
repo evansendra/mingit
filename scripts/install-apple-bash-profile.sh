423506662af0bceaeba38728920be96f1ca59661
Evan <code@evansendra.com>
2016-10-19 12:36:51 +0200

    stop addingt spaces to dot files

--- scripts/install-apple-bash-profile.sh
+++ scripts/install-apple-bash-profile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cp scripts/bash_profile ~/.mingit.sh
-echo "" >> ~/.bash_profile
 echo "source ~/.mingit.sh" >> ~/.bash_profile
--- scripts/install-linux-bashrc.sh
+++ scripts/install-linux-bashrc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cp scripts/bashrc ~/.mingit.sh
-echo "" >> ~/.bashrc
 echo "source ~/.mingit.sh" >> ~/.bashrc
